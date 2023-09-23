ec5681bf2d11daed4cc9d16b065dbce302546d3a
srdtrk <59252793+srdtrk@users.noreply.github.com>
2023-09-24 01:05:42 +0300

    Initial commit

---
+++ module/module.go
@@ -0,0 +1,117 @@
+package module
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"cosmossdk.io/core/appmodule"
+	gwruntime "github.com/grpc-ecosystem/grpc-gateway/runtime"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/cosmosregistry/example"
+	"github.com/cosmosregistry/example/keeper"
+)
+
+var (
+	_ module.AppModuleBasic = AppModule{}
+	_ module.HasGenesis     = AppModule{}
+	_ appmodule.AppModule   = AppModule{}
+)
+
+// ConsensusVersion defines the current module consensus version.
+const ConsensusVersion = 1
+
+type AppModule struct {
+	cdc    codec.Codec
+	keeper keeper.Keeper
+}
+
+// NewAppModule creates a new AppModule object
+func NewAppModule(cdc codec.Codec, keeper keeper.Keeper) AppModule {
+	return AppModule{
+		cdc:    cdc,
+		keeper: keeper,
+	}
+}
+
+func NewAppModuleBasic(m AppModule) module.AppModuleBasic {
+	return module.CoreAppModuleBasicAdaptor(m.Name(), m)
+}
+
+// Name returns the example module's name.
+func (AppModule) Name() string { return example.ModuleName }
+
+// RegisterLegacyAminoCodec registers the example module's types on the LegacyAmino codec.
+func (AppModule) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	example.RegisterLegacyAminoCodec(cdc)
+}
+
+// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the example module.
+func (AppModule) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *gwruntime.ServeMux) {
+	if err := example.RegisterQueryHandlerClient(context.Background(), mux, example.NewQueryClient(clientCtx)); err != nil {
+		panic(err)
+	}
+}
+
+// RegisterInterfaces registers interfaces and implementations of the example module.
+func (AppModule) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	example.RegisterInterfaces(registry)
+}
+
+// ConsensusVersion implements AppModule/ConsensusVersion.
+func (AppModule) ConsensusVersion() uint64 { return ConsensusVersion }
+
+// RegisterServices registers a gRPC query service to respond to the module-specific gRPC queries.
+func (am AppModule) RegisterServices(cfg module.Configurator) {
+	example.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
+	example.RegisterQueryServer(cfg.QueryServer(), keeper.NewQueryServerImpl(am.keeper))
+
+	// Register in place module state migration migrations
+	// m := keeper.NewMigrator(am.keeper)
+	// if err := cfg.RegisterMigration(example.ModuleName, 1, m.Migrate1to2); err != nil {
+	// 	panic(fmt.Sprintf("failed to migrate x/%s from version 1 to 2: %v", example.ModuleName, err))
+	// }
+}
+
+// DefaultGenesis returns default genesis state as raw bytes for the module.
+func (AppModule) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+	return cdc.MustMarshalJSON(example.NewGenesisState())
+}
+
+// ValidateGenesis performs genesis state validation for the circuit module.
+func (AppModule) ValidateGenesis(cdc codec.JSONCodec, _ client.TxEncodingConfig, bz json.RawMessage) error {
+	var data example.GenesisState
+	if err := cdc.UnmarshalJSON(bz, &data); err != nil {
+		return fmt.Errorf("failed to unmarshal %s genesis state: %w", example.ModuleName, err)
+	}
+
+	return data.Validate()
+}
+
+// InitGenesis performs genesis initialization for the example module.
+// It returns no validator updates.
+func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) {
+	var genesisState example.GenesisState
+	cdc.MustUnmarshalJSON(data, &genesisState)
+
+	if err := am.keeper.InitGenesis(ctx, &genesisState); err != nil {
+		panic(fmt.Sprintf("failed to initialize %s genesis state: %v", example.ModuleName, err))
+	}
+}
+
+// ExportGenesis returns the exported genesis state as raw bytes for the circuit
+// module.
+func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
+	gs, err := am.keeper.ExportGenesis(ctx)
+	if err != nil {
+		panic(fmt.Sprintf("failed to export %s genesis state: %v", example.ModuleName, err))
+	}
+
+	return cdc.MustMarshalJSON(gs)
+}
