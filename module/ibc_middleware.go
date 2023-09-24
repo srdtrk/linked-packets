8c0603c2a2ed5895d8a186ddbf559b4037412d66
srdtrk <srdtrk@hotmail.com>
2023-09-25 00:33:07 +0300

    feat: started building middleware

---
+++ module/ibc_middleware.go
@@ -0,0 +1,23 @@
+package module
+
+import (
+	porttypes "github.com/cosmos/ibc-go/v8/modules/core/05-port/types"
+
+	"github.com/srdtrk/linkedpackets/keeper"
+)
+
+// IBCMiddleware implements the ICS26 callbacks for the fee middleware given the
+// fee keeper and the underlying application.
+type IBCMiddleware struct {
+	app    porttypes.IBCModule
+	keeper keeper.Keeper
+}
+
+// NewIBCMiddleware creates a new IBCMiddlware given the keeper and underlying application
+func NewIBCMiddleware(app porttypes.IBCModule, k keeper.Keeper) IBCMiddleware {
+	return IBCMiddleware{
+		app:    app,
+		keeper: k,
+	}
+}
+
