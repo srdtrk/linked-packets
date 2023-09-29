0368504a41b6d0a3327a36ced165251de0f79da7
srdtrk <srdtrk@hotmail.com>
2023-09-30 01:01:18 +0300

    feat: added ibc_middleware.go and impleimented 'OnChanOpenInit'

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/cosmos/cosmos-proto v1.0.0-beta.3
 	github.com/cosmos/cosmos-sdk v0.50.0-rc.0.0.20230921103109-ceaf5b9b40f2
 	github.com/cosmos/gogoproto v1.4.11
+	github.com/cosmos/ibc-go/modules/capability v1.0.0-rc5
 	github.com/cosmos/ibc-go/v8 v8.0.0-beta.0
 	github.com/golang/protobuf v1.5.3
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
@@ -50,7 +51,6 @@ require (
 	github.com/cosmos/go-bip39 v1.0.0 // indirect
 	github.com/cosmos/gogogateway v1.2.0 // indirect
 	github.com/cosmos/iavl v1.0.0-rc.1 // indirect
-	github.com/cosmos/ibc-go/modules/capability v1.0.0-rc5 // indirect
 	github.com/cosmos/ics23/go v0.10.0 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.13.0 // indirect
 	github.com/danieljoos/wincred v1.1.2 // indirect
--- module/ibc_middleware.go
+++ module/ibc_middleware.go
@@ -1,10 +1,20 @@
 package module
 
 import (
+	"encoding/json"
 	"errors"
+	"strings"
 
+	"cosmossdk.io/collections"
+	errorsmod "cosmossdk.io/errors"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	capabilitytypes "github.com/cosmos/ibc-go/modules/capability/types"
+	channeltypes "github.com/cosmos/ibc-go/v8/modules/core/04-channel/types"
 	porttypes "github.com/cosmos/ibc-go/v8/modules/core/05-port/types"
 
+	"github.com/srdtrk/linkedpackets"
 	"github.com/srdtrk/linkedpackets/keeper"
 )
 
@@ -32,4 +42,56 @@ func NewIBCMiddleware(app porttypes.IBCModule, ics4Wrapper porttypes.ICS4Wrapper
 		ics4Wrapper: ics4Wrapper,
 		keeper:      k,
 	}
+}
+
+// OnChanOpenInit implements the IBCMiddleware interface
+func (im IBCMiddleware) OnChanOpenInit(
+	ctx sdk.Context,
+	order channeltypes.Order,
+	connectionHops []string,
+	portID string,
+	channelID string,
+	chanCap *capabilitytypes.Capability,
+	counterparty channeltypes.Counterparty,
+	version string,
+) (string, error) {
+	var versionMetadata linkedpackets.Metadata
+
+	if strings.TrimSpace(version) == "" {
+		// default version
+		versionMetadata = linkedpackets.Metadata{
+			LinkedPacketsVersion: linkedpackets.Version,
+			AppVersion:           "",
+		}
+	} else {
+		metadata, err := linkedpackets.MetadataFromVersion(version)
+		if err != nil {
+			// Since it is valid for fee version to not be specified, the above middleware version may be for a middleware
+			// lower down in the stack. Thus, if it is not a fee version we pass the entire version string onto the underlying
+			// application.
+			return im.app.OnChanOpenInit(ctx, order, connectionHops, portID, channelID,
+				chanCap, counterparty, version)
+		}
+		versionMetadata = metadata
+	}
+
+	if versionMetadata.LinkedPacketsVersion != linkedpackets.Version {
+		return "", errorsmod.Wrapf(linkedpackets.ErrInvalidVersion, "expected %s, got %s", linkedpackets.Version, versionMetadata.LinkedPacketsVersion)
+	}
+
+	appVersion, err := im.app.OnChanOpenInit(ctx, order, connectionHops, portID, channelID, chanCap, counterparty, versionMetadata.AppVersion)
+	if err != nil {
+		return "", err
+	}
+
+	versionMetadata.AppVersion = appVersion
+	versionBytes, err := json.Marshal(&versionMetadata)
+	if err != nil {
+		return "", err
+	}
+
+	im.keeper.LinkEnabled.Set(ctx, collections.Join(portID, channelID), true)
+
+	// call underlying app's OnChanOpenInit callback with the appVersion
+	return string(versionBytes), nil
 }
